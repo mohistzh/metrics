28958cc003a913eb0fd4591f87b6a4538021e61d
Z <mohistzh@gmail.com>
2019-12-23 15:25:48 +0800

    add rabbitmq queue metrics fetcher

--- metrics-transactor/src/main/java/me/mohistzh/metrics/transactor/fetcher/RabbitMQQueueFetcher.java
+++ metrics-transactor/src/main/java/me/mohistzh/metrics/transactor/fetcher/RabbitMQQueueFetcher.java
@@ -1,9 +1,9 @@
 package me.mohistzh.metrics.transactor.fetcher;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import lombok.extern.slf4j.Slf4j;
 import me.mohistzh.metrics.model.pojo.MetricsInstance;
-import me.mohistzh.metrics.model.pojo.RabbitMQRawMetrics;
 import me.mohistzh.metrics.transactor.feed.RabbitMQFeedManager;
 import org.apache.http.HttpHost;
 import org.apache.http.client.fluent.Executor;
@@ -12,32 +12,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
- * RabbitMQ node metrics <a>https://www.rabbitmq.com/monitoring.html#node-metrics</a>
+ * RabbitMQ Queue metrics, refer <a>https://www.rabbitmq.com/monitoring.html#queue-metrics</a>
+ *
  * @Author Jonathan
  * @Date 2019/12/23
  **/
 @Slf4j
 @Component
-public class RabbitMQFetcher implements BasicFetcher<RabbitMQRawMetrics>{
+public class RabbitMQQueueFetcher implements BasicFetcher<JSONArray>{
 
     @Autowired
     private RabbitMQFeedManager rabbitMQFeedManager;
 
-
     @Override
-    public RabbitMQRawMetrics fetch(MetricsInstance metricsInstance) {
-        // http configuration
+    public JSONArray fetch(MetricsInstance metricsInstance) {
         Executor executor = Executor.newInstance().auth(new HttpHost(metricsInstance.getHost(), metricsInstance.getPort()),
-                            metricsInstance.getUsername(), metricsInstance.getPassword());
+                metricsInstance.getUsername(), metricsInstance.getPassword());
 
         Request request = rabbitMQFeedManager.getMetricsConnector(metricsInstance);
         try {
             String content = executor.execute(request).returnContent().asString();
-            return JSON.parseObject(content, RabbitMQRawMetrics.class);
+            JSONArray jsonArray = JSON.parseArray(content);
+            return jsonArray;
         } catch (Exception e) {
-            log.error("Failed to retrieve RabbitMQ metrics.", e);
+            log.error("Failed to retrieve rabbitmq vhost metrics." ,e);
         }
-
         return null;
     }
 }
